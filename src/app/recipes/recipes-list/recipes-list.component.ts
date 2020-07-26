8438b844e323d832abf3c0717f8989db4d16a2bd
Badhon100 <badhonx102@gmail.com>
2020-07-27 00:32:49 +0600

    Designed recipe-list and shop-list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg') 
+  ];
   constructor() { }
 
   ngOnInit(): void {
